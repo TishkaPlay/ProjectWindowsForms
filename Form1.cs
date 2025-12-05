0b4fab67b2f760f101e5b61ddc5368f9b6f5eebb
TishkaaPlay <tikhon42otmyanin@gmail.com>
2025-12-05 08:44:59 +0300

    writing...

--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,57 @@ namespace SimpleTodoList
 {
     public partial class MainForm : Form
     {
+        //создаем список для заметок и где они будут хранится
+        private List<string> notes = new List<string>();
+        private string saveFile = "ThoughtSnap.txt";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        //нужно загрузить заметки из файла
+        private void LoadZam()
+        {
+            try
+            {
+                if (File.Exists(saveFile))
+                {
+                    //читаем все строки
+                    string[] lines = File.ReadAllLines(saveFile);
+
+                    notes.Clear();
+                    listBoxTasks.Items.Clear();
+
+                    foreach (string line in lines)
+                    {
+                        if (!string.IsNullOrEmpty(line))
+                        {
+                            notes.Add(line);
+                            listBoxTasks.Items.Add(GetShortNote(line));
+                        }
+                    }
+                }
+            }
+            catch 
+            {
+                
+            }
+        }
+
+        private void SaveZam()
+        {
+            try
+            {
+                //записываем все заметки в файл
+                File.WriteAllLines(saveFile, notes);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка! Не удалось сохранить: {ex.Message}");
+            }
+        }
+        
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
