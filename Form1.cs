9a056dd5111b4676d94f0e326e25c54c8aa8da90
TishkaaPlay <tikhon42otmyanin@gmail.com>
2025-12-05 08:55:33 +0300

    writing....

--- Form1.cs
+++ Form1.cs
@@ -35,7 +35,7 @@ namespace SimpleTodoList
                         if (!string.IsNullOrEmpty(line))
                         {
                             notes.Add(line);
-                            listBoxTasks.Items.Add(GetShortNote(line));
+                            listBoxTasks.Items.Add(GetShortZam(line));
                         }
                     }
                 }
@@ -58,7 +58,22 @@ namespace SimpleTodoList
                 MessageBox.Show($"Ошибка! Не удалось сохранить: {ex.Message}");
             }
         }
-        
+
+        //укоротили длинные заметки для отображения
+        private string GetShortZam(string note)
+        {
+            if (note.Length > 40)
+                return note.Substring(0, 37) + "...";
+            return note;
+        }
+
+        private void CountZam()
+        {
+            int count = notes.Count;
+
+            labelText.Text = $"Список заметок: {count}";
+        }
+
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
@@ -66,17 +81,41 @@ namespace SimpleTodoList
 
         private void buttonAdd_Click(object sender, EventArgs e)
         {
+            string newwords = textBox1.Text.Trim();
 
+            //пустая ли строка
+            if (string.IsNullOrEmpty(newwords))
+                MessageBox.Show("Введите текст заметки!");
+            return;
+
+            //добавляем заметку в список
+            notes.Add(newwords);
+            listBoxTasks.Items.Add(GetShortZam(newwords));
+
+            textBox1.Clear();
+            textBox1.Focus();
+
+            //обновляем и сохраняем
+            CountZam();
+            SaveZam();
         }
 
         private void listBoxTasks_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            //показываем полный текст выбранной заметки
+            if (listBoxTasks.SelectedIndex != -1)
+            {
+                textBox1.Text = notes[listBoxTasks.SelectedIndex];
+            }
         }
 
         private void buttonDelete_Click(object sender, EventArgs e)
         {
-
+            if (listBoxTasks.SelectedIndex == -1)
+            {
+                MessageBox.Show("Выберите заметку для удаления!");
+                return;
+            }
         }
 
         private void labelText_Click(object sender, EventArgs e)
